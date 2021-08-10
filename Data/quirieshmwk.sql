4849911edb278e1f849c9bd92c7c4f4461372c7b
GaryG484 <85652494+GaryG484@users.noreply.github.com>
2021-08-09 21:31:32 -0500

    Deliverable 2 Complete

--- Data/quirieshmwk.sql
+++ Data/quirieshmwk.sql
@@ -79,7 +79,8 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT first_name, last_name
+SELECT f
+irst_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
@@ -274,3 +275,67 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+CREATE TABLE dept_employees (
+emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+DROP TABLE emp_and_dep_emp
+--joining employees and department employees table
+SELECT employees.emp_no,
+     employees.first_name,
+     employees.last_name,
+	 employees.birth_date,
+	 dept_employees.from_date,
+	 dept_employees.to_date
+INTO emp_and_dep_emp
+FROM employees
+INNER JOIN dept_employees
+ON employees.emp_no = dept_employees.emp_no
+ORDER BY employees.emp_no
+;
+
+SELECT emp_and_dep_emp.emp_no,
+     emp_and_dep_emp.first_name,
+     emp_and_dep_emp.last_name,
+	 emp_and_dep_emp.birth_date,
+	emp_and_dep_emp.from_date,
+	 emp_and_dep_emp.to_date,
+	 titles.title
+INTO emp_and_dep_emp_and_title
+FROM emp_and_dep_emp
+INNER JOIN titles
+ON emp_and_dep_emp.emp_no = titles.emp_no
+ORDER BY emp_and_dep_emp.emp_no
+;
+
+DROP TABLE emp_and_dep_emp_and_title_clean
+SELECT emp_and_dep_emp_and_title.emp_no,
+     emp_and_dep_emp_and_title.first_name,
+     emp_and_dep_emp_and_title.last_name,
+	 emp_and_dep_emp_and_title.birth_date,
+	emp_and_dep_emp_and_title.from_date,
+	 emp_and_dep_emp_and_title.to_date,
+	 emp_and_dep_emp_and_title.title
+INTO emp_and_dep_emp_and_title_clean
+FROM emp_and_dep_emp_and_title
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (to_date BETWEEN '9999-01-01' AND '9999-01-01');
+;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+birth_date,
+from_date,
+to_date,
+title
+
+INTO mentorship_eligibility
+FROM emp_and_dep_emp_and_title_clean
+ORDER BY emp_no;
